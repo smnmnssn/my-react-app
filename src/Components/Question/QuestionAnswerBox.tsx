9aa19f6ec9b3c6643236a9349c6a8584e38ecf6b
Simon <simonmansson@live.se>
2025-02-20 14:42:34 +0100

    QuestionAnswerBox added to quizpage, added google font

--- src/Components/Question/QuestionAnswerBox.tsx
+++ src/Components/Question/QuestionAnswerBox.tsx
@@ -1,30 +1,33 @@
-
 export default function QuestionAnswerBox() {
   return (
-    <div className="flex flex-col border bg-yellow-400 w-100 h-100">
-      <label htmlFor="" className="text-2xl p-4 mt-10"> Category :
-        <select name="Category" className="ml-2 border bg-white hover:cursor-pointer" >
-          <option value="Javascript">JavaScript</option>
-          <option value="History">History</option>
-          <option value="Science">Science</option>
-        </select>
-      </label>
-      <label htmlFor="" className="text-2xl p-4 "> Difficulty :
-        <select name="Difficulty" className="ml-2 bg-white border hover:cursor-pointer" >
-          <option value="Normal">Normal</option>
-          <option value="Hard">Hard</option>
-        </select>
-      </label>
-      <div className="flex-1">
+    <>
+      <div className="flex flex-col border bg-yellow-400 w-100 h-100">
+        <div className="flex flex-col p-4 m-10 text-2xl">
+          <label className="p-4">
+            <input type="radio" name="myRadio" value="answer1" className="size-4"/> Answer
+          </label>
+          <label className="p-4">
+            <input type="radio" name="myRadio" value="answer2" className="size-4" /> Answer
+          </label>
+          <label className="p-4">
+            <input type="radio" name="myRadio" value="answer3" className="size-4"/> Answer
+          </label>
+        </div>
+        <div className="flex-1"></div>
+        <div className="flex justify-center mb-15">
+          <button
+            onClick={clickMe}
+            className="border w-20 bg-white h-10 hover:cursor-pointer"
+          >
+            Next
+          </button>
+        </div>
       </div>
-      <div className="flex justify-center mb-15">
-      <button onClick={clickMe} className="border w-40 bg-white h-10 hover:cursor-pointer" >Start Quiz</button>
-      </div>
-    </div>
+    </>
   );
 }
-
 
 function clickMe() {
   alert("You clicked me!");
 }
+
---
+++ src/Pages/QuizPage.tsx
@@ -0,0 +1,9 @@
+import QuestionAnswerBox from "../Components/Question/QuestionAnswerBox";
+
+export default function QuizPage() {
+  return (
+  <div className="flex justify-center">
+    <QuestionAnswerBox/>
+  </div>
+  )
+}
