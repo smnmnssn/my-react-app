a4a2d2b9d3b4f98b5c34c764aa7ac96a012511cf
Simon <simonmansson@live.se>
2025-02-20 14:58:59 +0100

    QuestionNumber added to QuizPage

---
+++ src/Components/Question/QuestionNumber.tsx
@@ -0,0 +1,8 @@
+export default function QuestionNumber() {
+  return (
+    <div className="flex justify-center">
+      <p className="text-3xl p-5">1/10</p>
+    </div>
+  )
+
+}
--- src/Pages/QuizPage.tsx
+++ src/Pages/QuizPage.tsx
@@ -1,11 +1,13 @@
 import QuestionAnswerBox from "../Components/Question/QuestionAnswerBox";
+import QuestionNumber from "../Components/Question/QuestionNumber";
 import QuestionTextBox from "../Components/Question/QuestionTextBox";
 
 export default function QuizPage() {
   return (
-  <div className="flex flex-col">
-    <QuestionTextBox />
-    <QuestionAnswerBox/>
-  </div>
-  )
+    <div className="flex flex-col">
+      <QuestionNumber />
+      <QuestionTextBox />
+      <QuestionAnswerBox />
+    </div>
+  );
 }
