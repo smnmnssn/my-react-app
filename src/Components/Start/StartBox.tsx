fb422de3e650d5255c6f686361d6aa87688660dc
Simon <simonmansson@live.se>
2025-02-24 15:01:01 +0100

    useState in StartBox.tsx

--- src/Components/Start/StartBox.tsx
+++ src/Components/Start/StartBox.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 export default function StartBox() {
+  const [category, setCategory] = useState("Javascript");
+  const [difficulty, setDifficulty] = useState("Normal");
+
+
   return (
     <div className="flex flex-col border bg-yellow-400 w-100 h-100">
       <label htmlFor="" className="text-2xl p-4 mt-10">
@@ -9,6 +14,8 @@ export default function StartBox() {
         <select
           name="Category"
           className="ml-2 border bg-white hover:cursor-pointer"
+          value = { category }
+          onChange = { (e) => setCategory(e.target.value)}
         >
           <option value="Javascript">JavaScript</option>
           <option value="History">History</option>
@@ -21,6 +28,8 @@ export default function StartBox() {
         <select
           name="Difficulty"
           className="ml-2 bg-white border hover:cursor-pointer"
+          value = { difficulty }
+          onChange = { (e) => setDifficulty(e.target.value)}
         >
           <option value="Normal">Normal</option>
           <option value="Hard">Hard</option>
@@ -37,5 +46,3 @@ export default function StartBox() {
     </div>
   );
 }
-
-
